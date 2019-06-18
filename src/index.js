bf71b55ac673d21e0fba9db284e525cbca3b92d0
gzaii1 <942096269@qq.com>
2019-06-18 19:44:53 +0800

    添加了游戏胜利判断

--- src/index.js
+++ src/index.js
@@ -2,34 +2,76 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// 第一版
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+// 方块组件 (受控组件)
+// class Square extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: null,
+//     };
+//   }
 
-  render() {
-    return (
-      <button 
-          className="square" 
-          onClick={()=> this.setState({value:'X'})}
+//   render() {
+//     return (
+//       <button 
+//           className="square" 
+//           onClick = {() => this.props.onClick()}
+//         >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+
+// 在不需要state时,用函数组件的形式更加快捷
+function Square(props) {
+  return (
+    <button 
+        className="square"
+        onClick={props.onClick}
         >
-        {this.state.value}
-      </button>
-    );
-  }
+        {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    // 每次修改值时,做一个原值的备份,然后修改备份的值,
+    // 将备份赋予原属性中,这样做的效果和直接修改的效果是一样的
+    // const squares = this.state.squares.slice();
+    const squares = [...this.state.squares];
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
   renderSquare(i) {
-    return <Square value = {i}/>;
+    return <Square 
+              value = {this.state.squares[i]}
+              onClick = {()=>this.handleClick(i)}
+              />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner:' + winner;
+    } else {
+      status = "Next player :" + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -70,6 +112,26 @@ class Game extends React.Component {
   }
 }
 
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 ReactDOM.render(
